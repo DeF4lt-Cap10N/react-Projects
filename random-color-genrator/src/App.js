d741682a3cf40879a50ec90c4525b560a87befcd
akash1675 <captainakash1675@gmail.com>
2024-10-16 00:21:55 +0530

    add color gerator

---
+++ random-color-genrator/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import RandomColor from './components/RandomColor';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomColor></RandomColor>
+    </div>
+  );
+}
+
+export default App;
