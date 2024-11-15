c73b8b474e61d587e81b863c383e861eda151507
akash1675 <captainakash1675@gmail.com>
2024-11-15 21:14:11 +0530

    add slider

---
+++ image-slider/src/App.jsx
@@ -0,0 +1,17 @@
+import ImageSlider from "./components/ImageSlider"
+
+function App() {
+
+
+  return (
+    <>
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
+    </>
+  )
+}
+
+export default App
---
+++ image-slider/src/components/ImageSlider.jsx
@@ -0,0 +1,50 @@
+
+import { useEffect, useState } from "react"
+
+
+function ImageSlider({ url, limit=5, page = 1 }) {
+
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit =${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data)
+        setLoading(false);
+      }
+    }
+    catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== '') fetchImages(url);
+  }, [url])
+
+  console.log(images);
+
+  if(loading){
+    return <div>Loding data! please wait</div>
+  }
+
+  if(errorMsg !== null){
+    return <div>Error occured! {errorMsg}</div>
+  }
+
+  return (
+    <div className="container">
+      hii
+    </div>
+  )
+}
+
+export default ImageSlider
