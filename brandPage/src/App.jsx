9c0f81bc08780f4cc0c906bec6955c2a5d4a676d
akash1675 <captainakash1675@gmail.com>
2025-02-08 21:02:32 +0530

    complete BrandPage

--- brandPage/src/App.jsx
+++ brandPage/src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar"
+import Banner from "./components/Banner"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <>
      <div>
       <Navbar></Navbar>
+      <Banner></Banner>
      </div>
     </>
   )
---
+++ brandPage/src/components/Banner.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import amzonIcon from "../assets/amazon.png"
+import flipkartIcon from "../assets/flipkart.png"
+import shoes from "../assets/shoe_image.png"
+
+const Banner = () => {
+   return (
+      <div style={{height:"617px", display:"flex", justifyContent:"center", alignItems:"center"}}>
+         {/* first box*/}
+         <div style={{width:"595px", height:"617px", display:"flex", flexDirection:"column", justifyContent:"center" }}>
+            <h1  style={{width:"595px", height:"306px", fontFamily:"sans-serif", fontSize:"90px", fontWeight:"800", marginBottom:'0px', lineHeight:"1"}}>
+               YOUR FEET DESERVE THE BEST 
+            </h1>
+            <p style={{width:"404px", height:"95px", fontFamily:"sans-serif", fontSize:"18px"}}>
+               YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+            </p>
+            <div style={{width:"331px", height:"41px", }}>
+               <button style={{width:"151px",height:"41px",  marginRight:"10px", backgroundColor:"red", outline:'none', border:"none", color:"white", fontFamily:"sans-serif", fontSize:"18px"}}>Shop Now</button>
+               <button style={{width:"151px",height:"41px",  marginRight:"10px", outline:'none',fontFamily:"sans-serif", fontSize:"18px"  }}>Category</button>
+            </div>
+            
+            <div style={{width:"131px", height:"67px",fontFamily:"sans-serif", fontSize:"15px"}}>
+               <p >Also Available on </p>
+               <div style={{width:"88px", height:"32px", display:"flex",gap:"9px", verticalAlign:"middle"}}>
+                    <img src={amzonIcon} alt="" />
+               <img src={flipkartIcon} alt="" />
+               </div>
+             
+            </div>
+            
+         </div>
+         {/* second box: image */}
+         <div style={{ width: "530px", height: "465px", display: "flex", alignItems: "center" }}>
+      <img src={shoes} alt="" />
+   </div>
+      </div>
+   )
+}
+
+export default Banner
