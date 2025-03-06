481d870543ff9175ab24335a9bd3d5be1c35d8f1
akash1675 <captainakash1675@gmail.com>
2025-03-06 21:10:43 +0530

    init toggle

---
+++ Toggle/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+    <div style={{display:"flex", justifyContent:"center",  }}>
+    
+    </div>
+    
+    </>
+  )
+}
+
+export default App
