2ffe7d8bd1d590876d14cd98de61a904e5d43d92
Rahul sharma <captainakash1675@gmail.com>
2025-03-16 22:35:10 +0530

    add toggle

--- Toggle/src/App.jsx
+++ Toggle/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import Toggle from "./components/Toggle"
 
 function App() {
 
   return (
     <>
     <div style={{display:"flex", justifyContent:"center",  }}>
-    
+    <Toggle/>
     </div>
     
     </>
